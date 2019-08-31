7b83d972913b82b408401b743c7ca28f4f74a729
radbrt <hho@cerx.co>
2019-08-31 20:51:23 +0200

    Documented main function

--- R/main_functions.R
+++ R/main_functions.R
@@ -10,6 +10,16 @@ match_number <- function(matchlist) {
   cr
 }
 
+#' Regex row matcher
+#'
+#' This function accepts a dataframe, regex-pattern and column name for definitions, and return matching rows
+#' @param df Sorted and grouped dataframe to filter
+#' @param ptn Perl-regex pattern to look for, based on the definitions created
+#' @param defs Column containing the definition of rows
+#' @keywords match_recognize
+#' @export
+#' @examples
+#' regex_row_matcher(df, "([D]{4,})", .$ds)
 regex_row_matcher <- function(df, ptn, defs) {
   
   defstring <- paste(defs, collapse='')
