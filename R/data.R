573c9d889960c68e1f3bce84a22e164b1ae1b378
radbrt <hho@cerx.co>
2019-09-15 12:25:29 +0200

    Data and documentation for use in code examples

--- R/data.R
+++ R/data.R
@@ -1,8 +1,39 @@
-#' This is data to be included in my package
-#'
+#' Stock data for three companies
+#' 
+#' A dataset containing stock prices for Amazon (AMZN), Microsoft (MSFT) and Apple (AAPL)
+#' for the period August 30st 2018 to August 30th 2019. The variables are:
+#' 
+#' \itemize{
+#'   \item date. Trading date (2018-08-30 - 2019-08-30)
+#'   \item open. Opening price.
+#'   \item high. High price during trading day.
+#'   \item low. Low price during trading day.
+#'   \item close. Closing price.
+#'   \item adj_close. Adjusted closing price.
+#'   \item volume. Trade volume.
+#'   \item ticker. Name of stock.
+#' }
+#' 
 #' @name stocks
 #' @docType data
 #' @author Radbrt \email{dev_null@@cerx.co}
-#' @references \url{yahoo.com}
-#' @keywords stock data
+#' @references \url{https://finance.yahoo.com/quotes/AMZN,AAPL,MSFT}
+#' @keywords datasets
+NULL
+
+#' Stock data for Microsoft
+#' 
+#' A subset of the `stocks` dataset, pre-sorted and with example definition columns for ease of use in examples.
+#' The new variables are:
+#' 
+#' \itemize{
+#'   \item change. Character variable cointaining "UP" if `adj_close` is higher than previous day, else "DOWN".
+#'   \item change2. Same as `change`, but containing only "U" and "D" for use with `match_rows_raw` function.
+#' }
+#'
+#' @name msft
+#' @docType data
+#' @author Radbrt \email{dev_null@@cerx.co}
+#' @references \url{https://finance.yahoo.com/quotes/AMZN,AAPL,MSFT}
+#' @keywords datasets
 NULL
--- R/frontend_functions.R
+++ R/frontend_functions.R
@@ -8,9 +8,7 @@
 #' @keywords match_recognize
 #' @export
 #' @examples
-#' \dontrun{
-#' match_rows_raw(stocks, ds, "([D]{4,})")
-#' }
+#' match_rows_raw(msft, change2, "([D]{4,})", mnum)
 match_rows_raw <- function(df, definitions, rx, match_name=NULL) {
   definitions <- enquo(definitions)
   match_name <- enquo(match_name)
@@ -28,9 +26,7 @@ match_rows_raw <- function(df, definitions, rx, match_name=NULL) {
 #' @keywords match_recognize
 #' @export
 #' @examples
-#' \dontrun{
-#' match_rows(df, my_definitions_col, "UP{4,} DOWN{4,}", match_number=mnum)
-#' }
+#' match_rows(msft, change, "UP{4,} DOWN{3,}", match_name=mnum)
 match_rows <- function(df, definitions, rx, match_name=NULL) {
   definitions <- enquo(definitions)
   match_name <- enquo(match_name)
